1681b016fbe3df752e81a8fb5c49d396ec00f629
MrBroooo <broooooomr@gmail.com>
2021-12-24 00:01:21 +0500

    kamchiliklar bartaraf etildi!

--- src/main/java/byfayzullayev/market/entity/Product.java
+++ src/main/java/byfayzullayev/market/entity/Product.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.*;
 
 
 @AllArgsConstructor
@@ -17,6 +14,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 @Entity
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String marketname;
